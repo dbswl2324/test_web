4a0a798a2f12cb235d4d66db5836c7608913b4ef
SANGHEE <tkdgmlgg7@gmail.com>
2022-04-18 11:23:15 +0900

    대문

---
+++ review/homeapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('index1/', views.index1, name="index"),
+    path('login/', auth_views.LoginView.as_view(template_name="testapp/login.html"), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
+    path('board/', views.board),
+]
