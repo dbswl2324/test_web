780cd27741c7e0f8e73c558fb83781942d8253db
dbswl2324 <leej23243@gmail.com>
2022-04-22 10:40:23 +0900

    update 기능변경

--- review/analysisapp/views.py
+++ review/analysisapp/views.py
@@ -6,14 +6,13 @@ from analysisapp.models import ArticleInfo, BuyList, ReviewData,ReviewAnalysis
 def show(request):
     return render(request, 'analysisapp/show.html') 
 
-#def index(request):
-#    return render(request, 'analysisapp/index.html') 
+
 
 def index(request):
      if 'article_code' in request.GET:
          item = get_object_or_404(ReviewAnalysis, article_code=request.GET.get('article_code'))
          
-         item2 = ReviewData.objects.all()
+         item2 = request.GET.get('article_code')
          row = ReviewData.objects.filter(article_code=request.GET.get('article_code'))
 
          item3=BuyList.objects.filter(article_code=request.GET.get('article_code')).order_by('buy_coin')
