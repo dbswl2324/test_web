3b64a8b486390727b1e2d7d12073cd9c55a6e18d
dbswl2324 <leej23243@gmail.com>
2022-04-26 13:33:05 +0900

    update analysis

--- review/analysisapp/views.py
+++ review/analysisapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
-from analysisapp.models import ArticleInfo
+from analysisapp.models import ArticleInfo,ReviewData,ReviewAnalysis
 # from analysisapp.models import  BuyList, ReviewData,ReviewAnalysis
 # Create your views here.
 
@@ -24,62 +24,69 @@ def search_main(request):
         
     return HttpResponseRedirect('/analysis/show/')
 
-def index(request):
-    if request.method == "GET":
-        print("get")
-        pass
-    elif request.method == "POST":
-        article_code= 1
-        
-        #검색된 게시물 이름
-        search_name=2
-
-        # 리뷰 게시물 개수
-        article_review_cnt= 3
-
-        #순수 리뷰 게시물 개수
-        article_pure_review_cnt= 4
-
-        #검색 카운트
-        search_cnt= 5
-
-        m = ArticleInfo(
-            article_code = article_code, 
-            search_name= search_name,
-            article_review_cnt=article_review_cnt,
-            article_pure_review_cnt=article_pure_review_cnt,
-            search_cnt=search_cnt
-        )
-        m.save()
-        print("Post")
-    return HttpResponseRedirect('/analysis/show')
-
 # def index(request):
-#     if 'article_code' in request.GET:
-#         item = get_object_or_404(ReviewAnalysis, article_code=request.GET.get('article_code'))
-#         print("스탭 1")
-#         item2 = request.GET.get('article_code')
-#         row = ReviewData.objects.filter(article_code=request.GET.get('article_code'))
-#         list = get_object_or_404(ArticleInfo, article_code=request.GET.get('article_code'))
-#         item3=BuyList.objects.filter(article_code=request.GET.get('article_code')).order_by('buy_coin')
-    
-#         return render(request, 'analysisapp/index.html', {'item': item,'item2':item2,'row':row,'item3':item3})
-
+#     if request.method == "GET":
+#         print("get")
+#         pass
 #     elif request.method == "POST":
-#         search_name = request.POST.get('search_name')
-#         print('가 ',search_name)
-#         list = get_object_or_404(ArticleInfo, search_name=request.POST.get('search_name'))
-#         print('2가 ',list)
-#         item = get_object_or_404(ReviewAnalysis, article_code=list.article_code)
-#         print('3가 ',item)
-#         row = ReviewData.objects.filter(article_code=list.article_code)
-#         print('4가 ',search_name)
-#         item2 = list.article_code
-#         print('5가 ',search_name)
-#         item3=BuyList.objects.filter(article_code=list.article_code).order_by('buy_coin')
-#         print('6가 ',search_name)
-#         return render(request, 'analysisapp/index.html', {'item': item,'item2':item2,'row':row,'item3':item3,'list':list})
+#         article_code= 1
+        
+#         #검색된 게시물 이름
+#         search_name=2
+
+#         # 리뷰 게시물 개수
+#         article_review_cnt= 3
+
+#         #순수 리뷰 게시물 개수
+#         article_pure_review_cnt= 4
+
+#         #검색 카운트
+#         search_cnt= 5
+
+#         m = ArticleInfo(
+#             article_code = article_code, 
+#             search_name= search_name,
+#             article_review_cnt=article_review_cnt,
+#             article_pure_review_cnt=article_pure_review_cnt,
+#             search_cnt=search_cnt
+#         )
+#         m.save()
+#         print("Post")
 #     return HttpResponseRedirect('/analysis/show')
+
+def index(request):
+    print("아무나1")
+    if request.method == "GET":
+        article_code = request.GET.get('article_code',False)
+        print("아무나2")
+        if article_code:
+            row = ReviewData.objects.filter(article_code=article_code)    
+            # for data in row:
+            # blog_code= row.blog_code
+            # print('ddddddd',blog_code)
+            # item = get_object_or_404(ReviewAnalysis, blog_code=blog_code)           
+            
+            # list = get_object_or_404(ArticleInfo, article_code=article_code)
+            #item3=BuyList.objects.filter(article_code=article_code).order_by('buy_coin')
+            return render(request, 'analysisapp/index.html', {'row':row})
+            #return render(request, 'analysisapp/index.html', {'item': item,'item2':article_code,'row':row})
+        else :
+            print("아무나3")
+            return HttpResponseRedirect('/app/home')
+    elif request.method == "POST":
+        search_name = request.POST.get('search_name',False)
+        print("아무나4")
+        if search_name:
+            list = get_object_or_404(ArticleInfo, search_name=search_name)
+            row = ReviewData.objects.filter(article_code=list.article_code)
+            # item = get_object_or_404(ReviewAnalysis, blog_code=row.blog_code)
+            item2 = list.article_code
+            #item3=BuyList.objects.filter(article_code=list.article_code).order_by('buy_coin')
+            return render(request, 'analysisapp/index.html', {'item': "Dasdas",'item2':item2,'row':row,'list':list})
+        else :
+            return render(request, 'analysisapp/show.html')
+    print("아무나")
+    return HttpResponseRedirect('app/analysis/show')
     
 from datetime import datetime
 def show(request):
